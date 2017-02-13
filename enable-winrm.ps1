2be097d53a9ad1fd703c8ee0a08406c1182e9961
Evgeny Khaliper <eugen.none@gmail.com>
2017-02-13 15:56:24 +0200

    Update enable-winrm.ps1

--- enable-winrm.ps1
+++ enable-winrm.ps1
@@ -1,4 +1,4 @@
 netsh advfirewall firewall add rule profile=any name="Allow WinRM Access" dir=in localport=5985 protocol=TCP action=allow
-winrm quickconfig -quiet
-winrm set winrm/config/service @{AllowUnencrypted="true"}
-winrm set winrm/config/service/Auth @{Basic="true"}
+cmd /c 'winrm quickconfig -quiet'
+cmd /c 'winrm set winrm/config/service @{AllowUnencrypted="true"}'
+cmd /c 'winrm set winrm/config/service/Auth @{Basic="true"}'
